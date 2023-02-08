f6135718d09c57b2223c6eb6214343fd9a9f763d
andremcnz1 <124759341+andremcnz1@users.noreply.github.com>
2023-02-08 18:08:20 +1300

    test commit

--- Assets/AIScript.cs
+++ Assets/AIScript.cs
@@ -7,7 +7,7 @@ public class AIScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // test
     }
 
     // Update is called once per frame
